d14a2d445affdcd294f7f549415a4018c8f3f889
evgen <evgeny15q@gmail.com>
2025-07-20 11:11:05 +0700

    Documentation PreparerMessageService

--- src/main/java/ru/telegrambot/telegram_bot_support/listener/service/message/PreparerMessageService.java
+++ src/main/java/ru/telegrambot/telegram_bot_support/listener/service/message/PreparerMessageService.java
@@ -5,7 +5,8 @@ import com.pengrad.telegrambot.request.SendPhoto;
 import org.springframework.stereotype.Service;
 import ru.telegrambot.telegram_bot_support.model.UserFollowing;
 
-import static ru.telegrambot.telegram_bot_support.constant.TelegramConstant.*;
+import static ru.telegrambot.telegram_bot_support.constant.InformationConstant.*;
+import static ru.telegrambot.telegram_bot_support.constant.TextMessageConstant.*;
 
 /**
  * сервис для подготовки фотографий и сообщений
@@ -14,7 +15,12 @@ import static ru.telegrambot.telegram_bot_support.constant.TelegramConstant.*;
 @Service
 public class PreparerMessageService {
 
-    public SendPhoto getStartPhotoMessage(long chatId, String firstNameUser) {
+    /**
+     * метод подготовки приветствующей фотографии
+     * @param chatId id чата пользователя
+     * @return SendPhoto приветствующую фотографию
+     */
+    public SendPhoto getStartPhotoMessage(long chatId) {
         java.io.File file = new java.io.File("src/main/resources/static/IMG_9145.PNG");
         SendPhoto photo = new SendPhoto(chatId, file);
         photo.caption(TEXT_INITIAL);
@@ -22,6 +28,14 @@ public class PreparerMessageService {
         return photo;
     }
 
+    /**
+     * метод подготовки текстового сообщения для администратора
+     * касательно информации и проверки оплаты пользователя
+     * @param chatId id чата пользователя
+     * @param firstNameUser имя пользователя
+     * @param username юзернэйм пользователя
+     * @return SendMessage текстовое сообщение
+     */
     public SendMessage getTextMessageToAdminForCheckingPayment(long chatId, String firstNameUser, String username) {
         return new SendMessage(
                 ADMIN_CHAT_ID,
@@ -34,16 +48,33 @@ public class PreparerMessageService {
                         + "\nвведите \"ДА\", если оплата прошла и \"НЕТ\", если оплата не прошла");
     }
 
+    /**
+     * метод подготовки текстового сообщения для пользователя об ожидании для проверки подлинности фото
+     * @param chatId id чата пользователя
+     * @return SendMessage текстовое сообщение
+     */
     public SendMessage getSendTextMessageToUserAboutGettingPhoto(long chatId) {
         return new SendMessage(
                 chatId,
-                "Фото получено на проверку! Пожалуйста, ожидайте");
+                TEXT_ABOUT_GETTING_PHOTO);
     }
 
+    /**
+     * метод подготовки текстового сообщения для администратора о добавлении пользователя в БД
+     * @param chatId id чата пользователя
+     * @return SendMessage текстовое сообщение
+     */
     public SendMessage getSendTextMessageToAdminAboutSaveUserToDB(long chatId) {
-        return new SendMessage(chatId, "Пользователь был успешно внесен в БД!");
+        return new SendMessage(
+                chatId,
+                "Пользователь был успешно внесен в БД!");
     }
 
+    /**
+     * метод подготовки текстового сообщения для пользователя об активации подписки
+     * @param chatId id чата пользователя
+     * @return SendMessage текстовое сообщение
+     */
     public SendMessage getSendTextMessageToUserAboutSuccessfulChecking(long chatId) {
         return new SendMessage(
                 chatId,
@@ -51,12 +82,22 @@ public class PreparerMessageService {
                 + "\nCрок подписки - 30 дней не зависимо от месяца");
     }
 
+    /**
+     * метод подготовки текстового сообщения для пользователя подписки о скором окончании подписки
+     * @param userFollowing пользователь с подпиской
+     * @return SendMessage текстовое сообщение
+     */
     public SendMessage sendMessageNotificationAboutApproachingEndSubscription(UserFollowing userFollowing) {
         return new SendMessage(
                 userFollowing.getChatId().toString(),
                 TEXT_NOTIFICATION);
     }
 
+    /**
+     * метод подготовки текстового сообщения для пользователя об окончании подписки
+     * @param userFollowing пользователь с подпиской
+     * @return SendMessage текстовое сообщение
+     */
     public SendMessage sendMessageNotificationAboutEndedSubscription(UserFollowing userFollowing) {
         return new SendMessage(
                 userFollowing.getChatId().toString(),
--- src/main/java/ru/telegrambot/telegram_bot_support/repository/UserRepository.java
+++ src/main/java/ru/telegrambot/telegram_bot_support/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package ru.telegrambot.telegram_bot_support.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.telegrambot.telegram_bot_support.model.UserFollowing;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserFollowing, Long> {
 
     public UserFollowing findByChatId(Long chatId);
